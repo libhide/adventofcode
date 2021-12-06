f270d86f7c8e8101a34dbb956f2ada5b5946350d
Ratik Sharma <ratik96@gmail.com>
2021-12-06 23:47:21 +0530

    update day 3 solution

--- src/main/kotlin/advent2021/Day03.kt
+++ src/main/kotlin/advent2021/Day03.kt
@@ -1,34 +1,27 @@
 package advent2021
 
 import Resources
-import kotlin.math.pow
 
 class Day03(private val input: List<String>) {
 
-    private val inputWidth = input.first().length
-
     fun solvePart1(): Int {
-        val gamma = (0 until inputWidth).sumOf { column ->
-            val columnValue = (inputWidth - column - 1).binaryColumnValue()
-            if (input.count { it[column] == '1' } > input.size / 2) columnValue else 0
-        }
-        return gamma * (inputWidth.maxBinaryValue() - gamma)
+        val gamma = input.first().indices.map { column ->
+            if (input.count { it[column] == '1' } > input.size / 2) '1' else '0'
+        }.joinToString("")
+
+        val epsilon = gamma.map { if(it == '1') '0' else '1' }.joinToString("")
+
+        return gamma.toInt(2) * epsilon.toInt(2)
     }
-
-    private fun Int.binaryColumnValue(): Int =
-        2.0.pow(this.toDouble()).toInt()
-
-    private fun Int.maxBinaryValue(): Int =
-        binaryColumnValue() - 1
 
     fun solvePart2(): Int =
         input.bitwiseFilter(true).toInt(2) * input.bitwiseFilter(false).toInt(2)
 
     private fun List<String>.bitwiseFilter(keepMostCommon: Boolean): String =
-        (0 until inputWidth).fold(this) { inputs, column ->
+        first().indices.fold(this) { inputs, column ->
             if (inputs.size == 1) inputs else {
                 val split = inputs.partition { it[column] == '1' }
-                if (keepMostCommon) split.longest() else split.shortest()
+                if(keepMostCommon) split.longest() else split.shortest()
             }
         }.first()
 
